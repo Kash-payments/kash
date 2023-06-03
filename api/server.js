d57aa2e6f48818fa8d2ea7b95b3d8545e83f4aad
Arturo GUerrero <arturo.guerrero@koibanx.com>
2023-06-02 19:17:48 -0600

    server

---
+++ api/server.js
@@ -0,0 +1,50 @@
+import express from "express"
+import bodyParser from "body-parser";
+import { transactions } from "./transactions.js"
+import SpaceAndTimeSDK from "./SpaceAndTimeSDK.js";
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+const initSDK = SpaceAndTimeSDK.init();
+
+app.get('/payments', async (req, res) => {
+  await getToken()
+  let token = initSDK.retrieveFileContents().accessToken
+  const result = await transactions.getAll(token)
+  return res.json(result)
+})
+
+app.post('/payments', async (req, res) => {
+  await getToken()
+  let token = initSDK.retrieveFileContents().accessToken
+  const result = await transactions.insert(token, req.body)
+  return res.json(result)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+async function getToken() {
+  let [validAccessTokenResponse, validAccessTokenError] = await initSDK.validateToken();
+  if (validAccessTokenResponse) {
+    console.log('Valid access token provided.');
+    console.log('Valid User ID: ', validAccessTokenResponse);
+  }
+  else {
+    let [refreshTokenResponse, refreshTokenError] = await initSDK.refreshToken();
+    console.log('Refreshed Tokens: ', refreshTokenResponse);
+
+    if (!refreshTokenResponse) {
+      let [tokenResponse, tokenError] = await initSDK.AuthenticateUser();
+      if (!tokenError) {
+        console.log(tokenResponse);
+      }
+      else {
+        console.log('Invalid User Tokens Provided');
+        console.log(tokenError);
+      }
+    }
+  }
+}
